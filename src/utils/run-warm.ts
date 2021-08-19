15cd0b0a8b779b41a3b08b8ea73ca1a7e2497e38
Jonathan Sibley <jonsibley@gmail.com>
2021-08-19 12:18:08 -0700

    log warmup function execution

--- src/utils/run-warm.ts
+++ src/utils/run-warm.ts
@@ -1,15 +1,14 @@
-const runWarm = (lambdaFunc: AWSLambda.Handler): AWSLambda.Handler => (
-  event,
-  context,
-  callback
-) => {
-  // Detect the keep-alive ping from CloudWatch and exit early. This keeps our
-  // lambda function running hot.
-  if (event.source === "serverless-plugin-warmup") {
-    return callback(null, "pinged");
-  }
+const runWarm =
+  (lambdaFunc: AWSLambda.Handler): AWSLambda.Handler =>
+  (event, context, callback) => {
+    // Detect the keep-alive ping from CloudWatch and exit early. This keeps our
+    // lambda function running hot.
+    if (event.source === "serverless-plugin-warmup") {
+      console.log("this invocation is just a warm-up ping");
+      return callback(null, "pinged");
+    }
 
-  return lambdaFunc(event, context, callback);
-};
+    return lambdaFunc(event, context, callback);
+  };
 
 export default runWarm;
